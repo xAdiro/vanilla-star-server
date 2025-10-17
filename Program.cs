2abe86f078a81db27c9125b47ca0237f85e0394a
xAdiro <adiro.rostek@gmail.com>
2025-10-18 01:18:01 +0200

    optimisation changes for prod

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddRazorPages();
-builder.Services.AddScoped<IMcServerStatusService,MineStatService>();
-builder.Services.AddScoped<IScoreboardReader,FNBTScoreboardReader>();
+builder.Services.AddScoped<IMcServerStatusService, MineStatService>();
+builder.Services.AddScoped<IScoreboardReader, FNBTScoreboardReader>();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
 
 // export ConnectionStrings__DefaultConnection="Host=192.168.0.3;Database=vanilla_scores;Username=vanilla_asterisk;Password=<password>"
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? throw new InvalidOperationException("No connectiotion string env variable found");
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("No connection string env variable found");
 
 builder.Services.AddDbContext<MinecraftDbContext>(options =>
         options.UseNpgsql(connectionString));
@@ -25,7 +30,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseResponseCompression();
 
 app.UseRouting();
 
